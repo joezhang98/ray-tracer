d400b32368fff413f183ce03615507edb69e3dce
Zhao Zhang <zz98@stanford.edu>
2021-03-13 12:10:37 -0800

    prog: added basic sphere object to ray tracer

--- vec3.h
+++ vec3.h
@@ -78,7 +78,7 @@ inline vec3 operator*(const vec3 &v, float t) {
 }
 
 inline float dot(const vec3 &v1, const vec3 &v2) {
-    return v1.e[0] * v2.e[0] + v1.e[1] + v2.e[1] + v1.e[2] * v2.e[2];
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
 
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
