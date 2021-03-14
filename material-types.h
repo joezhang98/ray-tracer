780f26193db817a1731702ad2a18cfb3acabdb96
Zhao Zhang <zz98@stanford.edu>
2021-03-13 22:02:25 -0800

    prog: added refract() function for dielectric materials

--- material-types.h
+++ material-types.h
@@ -20,6 +20,22 @@ vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n) * n;
 }
 
+/* Given an incident ray with direction V, a surface normal N, and
+   a ratio of refractive indices NI_OVER_NT, determines the direction
+   of the refracted ray (if any), and stores it in REFRACTED. Returns
+   true if there is a refracted ray and false otherwise. */
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt*dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt * (uv - n*dt) - n * sqrt(discriminant);
+        return true;
+    }
+    else
+        return false;
+}
+
 /*
    A diffuse material.
 */
